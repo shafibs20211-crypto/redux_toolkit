9026d5c135c715bf97e3f8b548f948a96264c1b5
shafibs20211-crypto <shafibs20211@gmail.com>
2026-09-15 10:16:48 -0700

    redux_Toolkit
    redux_Toolkit

---
+++ src/reducers/todoSlice.js
@@ -0,0 +1,43 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [
+    {
+      id: 1,
+      text: "learn Redux Toolkit",
+      completed: false,
+    },
+    {
+      id: 2,
+      text: "Buy Milk",
+      completed: false,
+    },
+  ],
+};
+
+export const todoSlice = createSlice({
+  name: "todos",
+  initialState,
+
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+        completed: false,
+      };
+
+      state.todos.push(todo);
+    },
+
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter(
+        (todo) => todo.id !== action.payload
+      );
+    },
+  },
+});
+
+export const { addTodo, removeTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
